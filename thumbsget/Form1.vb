d022b62d6bff49abbd5e5554e8ea437d1a7f2a66
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-11-01 19:00:33 -0400

    Add boilerplate.

--- thumbsget/Form1.vb
+++ thumbsget/Form1.vb
@@ -1,3 +1,33 @@
-﻿Public Class aaformMainWindow
+﻿'thumbsget - Gets the maxresdefault thumbnail for YouTube videos from
+'original video URL. Not associated With YouTube or Google/Alphabet.
+'Copyright (C) 2018 Drew Naylor
+'YouTube and all related words are copyright
+'and trademark Google/Alphabet. Also mentioned in About window.
+'Google/Alphabet is not affiliated with either the thumbsget project or Drew Naylor
+'and does not endorse this software.
+'Any other companies mentioned own their respective copyrights/trademarks.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+'This file is part of thumbsget.
+'
+'thumbsget is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'thumbsget is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with thumbsget.  If not, see <http://www.gnu.org/licenses/>.
 
+
+
+
+Public Class aaformMainWindow
+    Private Sub buttonGetThumbnailInBrowser_Click(sender As Object, e As EventArgs) Handles buttonGetThumbnailInBrowser.Click
+
+    End Sub
 End Class
