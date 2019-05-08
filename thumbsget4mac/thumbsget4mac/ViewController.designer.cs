697dd340b32da2bbcd8851c8902c3692a3878592
DrewNaylor <DrewNaylor@users.noreply.github.com>
2019-05-08 00:07:41 -0400

    Changing UI stuff...
    and adding an action to the preview button.

--- thumbsget4mac/thumbsget4mac/ViewController.designer.cs
+++ thumbsget4mac/thumbsget4mac/ViewController.designer.cs
@@ -1,17 +1,21 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
+// This file has been generated automatically by Visual Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace test
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Action ("buttonViewThumbnailInBrowser:")]
+        partial void buttonViewThumbnailInBrowser (Foundation.NSObject sender);
+        
+        void ReleaseDesignerOutlets ()
         {
         }
     }
