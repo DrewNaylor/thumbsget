c08de55aaef3c64fe90dc5a24961043c78a769ab
DrewNaylor <DrewNaylor@users.noreply.github.com>
2019-05-08 01:21:12 -0400

    Working on switching the code over to C#.
    It may take a bit to change it all over.

--- thumbsget4mac/thumbsget4mac/ViewController.cs
+++ thumbsget4mac/thumbsget4mac/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Diagnostics;
 using AppKit;
 using Foundation;
 
@@ -45,58 +45,58 @@ namespace thumbsget4mac
             {
             // Look at the URL, and replace "www.youtube.com/watch?v=" with "i.ytimg.com/vi/".
             //Actually, first replace "www." with "" (nothing).
-            Dim thumbnailUrl As String = textboxVideoUrl.Text
-            Debug.WriteLine("Start:")
-            Debug.WriteLine("Current URL: " & thumbnailUrl)
+            string thumbnailUrl = textboxVideoUrl.StringValue;
+                Debug.WriteLine("Start:");
+                Debug.WriteLine("Current URL: " + thumbnailUrl);
 
-            // Replace "www." with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("www.", "")
-            Debug.WriteLine("Replace www. Current URL: " & thumbnailUrl)
+                // Replace "www." with nothing.
+                thumbnailUrl = thumbnailUrl.Replace("www.", "");
+                Debug.WriteLine("Replace www. Current URL: " + thumbnailUrl);
 
-            // Replace "m." with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("m.", "")
-            Debug.WriteLine("Replace m. Current URL: " & thumbnailUrl)
+                // Replace "m." with nothing.
+                thumbnailUrl = thumbnailUrl.Replace("m.", "");
+                Debug.WriteLine("Replace m. Current URL: " + thumbnailUrl);
 
-            // Replace "?feature=youtu.be" with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("?feature=youtu.be", "")
-            Debug.WriteLine("Replace ?feature=youtu.be. Current URL: " & thumbnailUrl)
+                // Replace "?feature=youtu.be" with nothing.
+                thumbnailUrl = thumbnailUrl.Replace("?feature=youtu.be", "");
+            Debug.WriteLine("Replace ?feature=youtu.be. Current URL: " + thumbnailUrl);
 
-            // Replace "&feature=youtu.be" with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("&feature=youtu.be", "")
-            Debug.WriteLine("Replace &feature=youtu.be. Current URL: " & thumbnailUrl)
+                // Replace "&feature=youtu.be" with nothing.
+                thumbnailUrl = thumbnailUrl.Replace("&feature=youtu.be", "");
+                Debug.WriteLine("Replace &feature=youtu.be. Current URL: " + thumbnailUrl);
 
-            // Replace "&feature=share" with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("&feature=share", "")
-            Debug.WriteLine("Replace &feature=share. Current URL: " & thumbnailUrl)
+                // Replace "&feature=share" with nothing.
+                thumbnailUrl = thumbnailUrl.Replace("&feature=share", "");
+                Debug.WriteLine("Replace &feature=share. Current URL: " + thumbnailUrl);
 
             // Now, if "youtube.com/watch?" is in the URL, replace it and "v=" with "i.ytimg.com/vi/".
             If thumbnailUrl.Contains("youtube.com/watch?") Then
                 thumbnailUrl = thumbnailUrl.Replace("youtube.com/watch?v=", "i.ytimg.com/vi/")
-                Debug.WriteLine("Replace youtube.com/watch?v=. Current URL: " & thumbnailUrl)
+                Debug.WriteLine("Replace youtube.com/watch?v=. Current URL: " + thumbnailUrl)
             ElseIf thumbnailUrl.Contains("youtu.be/") Then
                 // If the URL contains "youtu.be/", replace that with "i.ytimg.com/vi/".
                 thumbnailUrl = thumbnailUrl.Replace("youtu.be/", "i.ytimg.com/vi/")
-                Debug.WriteLine("Replace youtu.be. Current URL: " & thumbnailUrl)
+                Debug.WriteLine("Replace youtu.be. Current URL: " + thumbnailUrl)
             End If
 
             // Replace "watch" with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("watch", "")
-            Debug.WriteLine("Replace watch. Current URL: " & thumbnailUrl)
+                thumbnailUrl = thumbnailUrl.Replace("watch", "");
+                Debug.WriteLine("Replace watch. Current URL: " + thumbnailUrl);
 
-            // Replace "&v=" with nothing.
-            thumbnailUrl = thumbnailUrl.Replace("&v=", "")
-            Debug.WriteLine("Replace &v=. Current URL: " & thumbnailUrl)
+                // Replace "&v=" with nothing.
+                thumbnailUrl = thumbnailUrl.Replace("&v=", "");
+                Debug.WriteLine("Replace &v=. Current URL: " + thumbnailUrl);
 
-            // Once again, replace "youtube.com/" with "i.ytimg.com/vi/".
-            thumbnailUrl = thumbnailUrl.Replace("youtube.com/", "i.ytimg.com/vi/")
-            Debug.WriteLine("Replace youtube.com/. Current URL: " & thumbnailUrl)
+                // Once again, replace "youtube.com/" with "i.ytimg.com/vi/".
+                thumbnailUrl = thumbnailUrl.Replace("youtube.com/", "i.ytimg.com/vi/");
+            Debug.WriteLine("Replace youtube.com/. Current URL: " + thumbnailUrl);
 
             // Add "maxresdefault.jpg" to end of URL.
-            thumbnailUrl = thumbnailUrl & "/maxresdefault.jpg"
-            Debug.WriteLine("Append /maxresdefault.jpg. Current URL: " & thumbnailUrl)
+                thumbnailUrl = thumbnailUrl + "/maxresdefault.jpg";
+                Debug.WriteLine("Append /maxresdefault.jpg. Current URL: " + thumbnailUrl);
 
-            // Return the thumbnail URL if the textbox isn't empty.
-            Return thumbnailUrl
+                // Return the thumbnail URL if the textbox isn't empty.
+                return thumbnailUrl;
         Else
 
             // If the textbox for the video URL is empty, just return the thumbnail of
