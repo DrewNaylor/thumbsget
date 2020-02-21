856591666866aa5ac15c78fb9d7150b9cd54dd97
DrewNaylor <DrewNaylor@users.noreply.github.com>
2020-02-21 18:11:22 -0500

    Properly hook up actions to buttons.

--- thumbsget4gtk/src/thumbsget4py.py
+++ thumbsget4gtk/src/thumbsget4py.py
@@ -3,37 +3,84 @@
 #File based on this StackOverflow answer:
 #https://stackoverflow.com/a/57455040
 
+#Also based on this Gnome demo:
+#https://developer.gnome.org/gnome-devel-demos/stable/toolbar_builder.py.html.en
+
 
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+import sys
 
 
 UI_FILE = "aaformMainWindow.glade"
 
-class GUI:
-    def __init__(self):
+class GUI(Gtk.ApplicationWindow):
+    def __init__(self, app):
+        Gtk.Window.__init__(self, title="thumbsget4py", application=app)
+        self.set_default_size(525, 204)
 
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file(UI_FILE)
-        self.builder.connect_signals(self)
 
-        window = self.builder.get_object('aaformMainWindow')
-        window.show()
+        # A builder to connect the UI to the app.
+        builder = Gtk.Builder()
 
-    def on_window_destroy(self, window):
+        # Attempt to get the main window file if it exists.
+        try:
+            builder.add_from_file("aaformMainWindow.glade")
+        except:
+            print("Main window file not found.")
+            sys.exit()
+
+        # Now put it in the window.
+        self.add(builder.get_object("boxMainWindow"))
+
+
+        # Get buttons that will be used later.
+        self.buttonViewThumbnailInBrowser = builder.get_object("buttonViewThumbnailInBrowser")
+        self.buttonPreviewThumbnail = builder.get_object("buttonPreviewThumbnail")
+        self.buttonSaveThumbnail = builder.get_object("buttonSaveThumbnail")
+        self.buttonAbout = builder.get_object("buttonAbout")
+
+        # Create actions that are used when clicking buttons
+        # connect their signal to a callback method, and add
+        # the action to the window.
+
+        # View thumbnail in browser.
+        self.buttonViewThumbnailInBrowser.connect("clicked", self.view_thumbnail_in_browser_callback)
+
+        # Preview thumbnail.
+        self.buttonPreviewThumbnail.connect("clicked", self.preview_thumbnail_callback)
+
+        # Save thumbnail.
+        self.buttonSaveThumbnail.connect("clicked", self.save_thumbnail_callback)
+
+
+    def preview_thumbnail_callback(self, widget):
+        print("Thumbnail previewed.")
+
+    def save_thumbnail_callback(self, widget):
+        print("Thumbnail saved.")
+
+    def view_thumbnail_in_browser_callback(self, widget):
+        print("Thumbnail opened in browser.")
+
+"""     def on_window_destroy(self, app):
         Gtk.main_quit()
+ """
 
-    def home_clicked (self, button):
-        stack = self.builder.get_object('stack')
-        home_button = self.builder.get_object('home_button')
-        stack.set_visible_child(home_button)
+class Application(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self)
 
-    def button_clicked (self, button):
-        stack = self.builder.get_object('stack')
-        notebook_box = self.builder.get_object('notebook_box')
-        stack.set_visible_child(notebook_box)
+    def do_activate(self):
+        win = GUI(self)
+        # Now we show the window with show()
+        win.show()
 
-app = GUI()
-Gtk.main()
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+app = Application()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
