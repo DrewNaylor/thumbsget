228e8fcd1c8c586331dd2d8051e1940ff887357f
DrewNaylor <DrewNaylor@users.noreply.github.com>
2020-02-21 19:31:11 -0500

    Properly get thumbnail URL after modifying the JS code to be Python.

--- thumbsget4gtk/src/thumbsget4py.py
+++ thumbsget4gtk/src/thumbsget4py.py
@@ -41,6 +41,8 @@ class GUI(Gtk.ApplicationWindow):
         self.buttonPreviewThumbnail = builder.get_object("buttonPreviewThumbnail")
         self.buttonSaveThumbnail = builder.get_object("buttonSaveThumbnail")
         self.buttonAbout = builder.get_object("buttonAbout")
+        # Get video URL textbox.
+        self.textboxVideoUrl = builder.get_object("textboxVideoUrl")
 
         # Create actions that are used when clicking buttons
         # connect their signal to a callback method, and add
@@ -57,7 +59,7 @@ class GUI(Gtk.ApplicationWindow):
 
 
     def preview_thumbnail_callback(self, widget):
-        print("Thumbnail previewed.")
+        print(thumbnailUrl.getThumbnailUrl(self, self.textboxVideoUrl.get_text()))
 
     def save_thumbnail_callback(self, widget):
         print("Thumbnail saved.")
@@ -76,6 +78,75 @@ class Application(Gtk.Application):
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
+
+class thumbnailUrl:
+    def getThumbnailUrl(self, thumbnailUrl):
+            # First we need to define the image box so we can set
+            # the preview in it.
+            print("Video URL: " + thumbnailUrl)
+            # Next, we need to make sure there's stuff in the textbox.
+            if thumbnailUrl != "None":
+            # Now we can do stuff and replace text.
+            
+                # Look at the URL, and replace "www.youtube.com/watch?v=" with "i.ytimg.com/vi/".
+                # Actually, first replace "www." with "" (nothing).
+                print("Start:")
+                print("Current URL: " + thumbnailUrl)
+
+                # Replace "www." with nothing.
+                thumbnailUrl = thumbnailUrl.replace("www.", "")
+                print("Replace www. Current URL: " + thumbnailUrl)
+
+                # Replace "m." with nothing.
+                thumbnailUrl = thumbnailUrl.replace("m.", "")
+                print("Replace m. Current URL: " + thumbnailUrl)
+
+                # Replace "?feature=youtu.be" with nothing.
+                thumbnailUrl = thumbnailUrl.replace("?feature=youtu.be", "")
+                print("Replace ?feature=youtu.be. Current URL: " + thumbnailUrl)
+
+                # Replace "&feature=youtu.be" with nothing.
+                thumbnailUrl = thumbnailUrl.replace("&feature=youtu.be", "")
+                print("Replace &feature=youtu.be. Current URL: " + thumbnailUrl)
+
+                # Replace "&feature=share" with nothing.
+                thumbnailUrl = thumbnailUrl.replace("&feature=share", "")
+                print("Replace &feature=share. Current URL: " + thumbnailUrl)
+
+                # Now, if "youtube.com/watch?" is in the URL, replace it and "v=" with "i.ytimg.com/vi/".
+                if ("youtube.com/watch?" in thumbnailUrl):
+                    thumbnailUrl = thumbnailUrl.replace("youtube.com/watch?v=", "i.ytimg.com/vi/")
+                    print("Replace youtube.com/watch?v=. Current URL: " + thumbnailUrl)
+                elif ("youtu.be/" in thumbnailUrl):
+                # If the URL contains "youtu.be/", replace that with "i.ytimg.com/vi/".
+                    thumbnailUrl = thumbnailUrl.replace("youtu.be/", "i.ytimg.com/vi/")
+                    print("Replace youtu.be. Current URL: " + thumbnailUrl)
+                # Replace "watch" with nothing.
+                thumbnailUrl = thumbnailUrl.replace("watch", "")
+                print("Replace watch. Current URL: " + thumbnailUrl)
+                # Replace "&v=" with nothing.
+                thumbnailUrl = thumbnailUrl.replace("&v=", "")
+                print("Replace &v=. Current URL: " + thumbnailUrl)
+                # Once again, replace "youtube.com/" with "i.ytimg.com/vi/".
+                thumbnailUrl = thumbnailUrl.replace("youtube.com/", "i.ytimg.com/vi/")
+                print("Replace youtube.com/. Current URL: " + thumbnailUrl)
+                 # Add "hqdefault.jpg" to end of URL if the
+                # # checkbox to use hqdefault instead is checked.
+                #     if(checkboxUseHQDefault.checked == 1):
+                #         thumbnailUrl = thumbnailUrl + "/hqdefault.jpg"
+                #         print("Append /hqdefault.jpg. Current URL: " + thumbnailUrl)
+                #     else:
+                    # Otherwise, if the checkbox is unchecked,
+                    # use maxresdefault.
+                thumbnailUrl = thumbnailUrl + "/maxresdefault.jpg"
+                print("Append /maxresdefault.jpg. Current URL: " + thumbnailUrl)
+                # Return the thumbnail URL if the textbox isn't empty and onlyReturnVideoID
+                return thumbnailUrl
+            else:
+            # If the textbox for the video URL is empty, just return the thumbnail of
+            # one of my Luigi's Mansion episodes.
+                return "https://i.ytimg.com/vi/kK7TJIMab7g/maxresdefault.jpg"
+                
 
 app = Application()
 exit_status = app.run(sys.argv)
